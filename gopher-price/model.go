7518cd2cb0373960f669333d226246eb4ad2ec4c
vincentlalo-long <nickmoi3056@gmail.com>
2026-03-02 23:37:16 +0700

    feat : interface for multi coin

--- gopher-price/model.go
+++ gopher-price/model.go
@@ -1,31 +1,71 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
-type BinanceResponse struct {
-	Symbol string `json:"symbol"`
-	Price  string `json:"price"`
+type PriceParser interface {
+	Parse(data []byte) (string, error)
 }
+type BinanceHandler struct{}
 
-type KuCoinResponse struct {
-	Data struct {
+func (h BinanceHandler) Parse(data []byte) (string, error) {
+	var resp struct {
 		Price string `json:"price"`
-	} `json:"data"`
+	}
+	err := json.Unmarshal(data, &resp)
+	return resp.Price, err
 }
 
-type BybitResponse struct {
-	Result []struct {
-		Price string `json:"lastPrice"`
-	} `json:"result"`
+type KuCoinHandler struct{}
+
+func (h KuCoinHandler) Parse(data []byte) (string, error) {
+	var resp struct {
+		Data struct {
+			Price string `json:"price"`
+		} `json:"data"`
+	}
+	err := json.Unmarshal(data, &resp)
+	return resp.Data.Price, err
 }
 
-//Assign task for worker
+type BybitHandler struct{}
+
+func (h BybitHandler) Parse(data []byte) (string, error) {
+	var resp struct {
+		Result struct {
+			List []struct {
+				LastPrice string `json:"lastPrice"`
+			} `json:"list"`
+		} `json:"result"`
+	}
+	err := json.Unmarshal(data, &resp)
+	if err != nil {
+		return "", err
+	}
+	if len(resp.Result.List) > 0 {
+		return resp.Result.List[0].LastPrice, err
+	}
+	return "", fmt.Errorf("No result")
+
+}
+
+var Handlers = map[string]PriceParser{
+	"Binance":     BinanceHandler{},
+	"Binance-ETH": BinanceHandler{},
+	"KuCoin":      KuCoinHandler{},
+	"ByBit":       BybitHandler{},
+}
+
+// Assign task for worker
 type PriceJob struct {
 	Exchange string
 	URL      string
 }
 
-//Return
+// Return
 type PriceResult struct {
 	Exchange string
 	Price    string
--- gopher-price/worker.go
+++ gopher-price/worker.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"io"
 	"net/http"
 	"sync"
 	"time"
@@ -11,39 +11,26 @@ func fetchPriceWorker(id int, jobs <-chan PriceJob, results chan<- PriceResult, 
 	defer wg.Done()
 
 	client := http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: 60 * time.Second,
 	}
 	for job := range jobs {
 		start := time.Now()
 		resp, err := client.Get(job.URL)
 		result := PriceResult{Exchange: job.Exchange, Err: err}
 		if err == nil {
-			// Xử lý JSON từ các exchange khác nhau
-			if job.Exchange == "Binance" || job.Exchange == "Binance-ETH" {
-				var data BinanceResponse
-				decodeErr := json.NewDecoder(resp.Body).Decode(&data)
-				if decodeErr == nil {
-					result.Price = data.Price
-				} else {
-					result.Err = decodeErr
-				}
-			} else if job.Exchange == "KuCoin" {
-				var data KuCoinResponse
-				decodeErr := json.NewDecoder(resp.Body).Decode(&data)
-				if decodeErr == nil {
-					result.Price = data.Data.Price
-				} else {
-					result.Err = decodeErr
-				}
-			} else if job.Exchange == "Bybit" {
-				var data BybitResponse
-				decodeErr := json.NewDecoder(resp.Body).Decode(&data)
-				if decodeErr == nil && len(data.Result) > 0 {
-					result.Price = data.Result[0].Price
-				} else {
-					result.Err = decodeErr
-				}
+			//encode json direct from response
+
+			body, _ := io.ReadAll(resp.Body)
+			resp.Body.Close()
+
+			if handler, ok := Handlers[job.Exchange]; ok {
+				price, parseErr := handler.Parse(body)
+				result.Price = price
+				result.Err = parseErr
+			} else {
+				result.Err = err
 			}
+
 		}
 		result.Duration = time.Since(start)
 		results <- result
